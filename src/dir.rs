bdd1e3bc672128b25b91bf432f745537c83e19cf
Jayden Pyles <jpylesbusiness@gmail.com>
2023-11-30 10:13:16 -0600

    feat: add create file if not exists

--- src/dir.rs
+++ src/dir.rs
@@ -1,4 +1,6 @@
 use std::fs::create_dir_all;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::Path;
 
 pub fn create_dir_if_not_exists(dir: &Path) {
@@ -11,6 +13,20 @@ pub fn create_dir_if_not_exists(dir: &Path) {
             Err(error) => {
                 println!("Error creating directory!");
             }
+        }
+    }
+}
+
+pub fn create_file_if_not_exists(file_path: &Path, contents: &str) {
+    let f = file_path.clone();
+
+    if !f.exists() {
+        let mut toml = File::create(file_path).expect("Could not create file!");
+        match toml.write_all(contents.as_bytes()) {
+            Err(_) => {
+                panic!("Error writing to config file.")
+            }
+            Ok(_) => {}
         }
     }
 }
